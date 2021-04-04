c2250d9a8df50ab88f93a68d169fb17cfd28a635
Graham Robertson <graham@grahamcracker.dev>
2021-04-03 23:30:05 -0500

    playlistmanager tests

--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
 const { expect } = require('chai');
 const Database = require('../db/database');
 const API = require('../spotify/api');
+const PlaylistManager = require('../spotify/playlistmgr');
+const dummyAlbum = require('./album');
 const dotenv = require('dotenv').config();
 
 // DATABASE
@@ -204,6 +206,170 @@ describe('API', function () {
       expect(response).to.have.property('token_type', 'Bearer');
 
       if (response.access_token) await db.setAccess(response.access_token);
+    });
+  });
+});
+
+// PLAYLISTMANAGER
+describe('PlaylistManager', function () {
+  const pm = new PlaylistManager();
+
+  describe('#parse()', function () {
+    const dummyRes = {
+      type: 'playlist',
+      id: '1',
+    };
+
+    const dummyErr = {
+      type: 'error',
+      error: 'some_error',
+    };
+
+    it('should parse body', function () {
+      expect(PlaylistManager.parse(dummyRes)).to.have.property('id', '1');
+    });
+
+    it('should parse error', function () {
+      expect(PlaylistManager.parse(dummyErr)).to.have.property('error');
+    });
+  });
+
+  describe('#extract()', function () {
+    const track =
+      'https://open.spotify.com/track/5rg2cr5aokhrm5gQNpyHXi?si=YBgz1m4IRmC6d1sW6KV6Hw';
+    const album =
+      'https://open.spotify.com/album/22dulUCoaoKoVpmg2irQmo?si=EZNBCCD_QN2adQnHUgCfKg';
+    const trackId = '5rg2cr5aokhrm5gQNpyHXi';
+    const albumId = '22dulUCoaoKoVpmg2irQmo';
+
+    it('should extract track id', function () {
+      expect(PlaylistManager.extractID(track)).to.equal(trackId);
+    });
+
+    it('should extract album id', function () {
+      expect(PlaylistManager.extractID(album)).to.equal(albumId);
+    });
+  });
+
+  describe('#extractURIs()', function () {
+    const needle = 'spotify:track:2TpxZ7JUBn3uw46aR7qd6V';
+    const tracks = PlaylistManager.extractURIs(dummyAlbum);
+    it('should return array', function () {
+      expect(tracks).to.be.a('array');
+    });
+
+    it('should include song URIs', function () {
+      expect(tracks[0]).to.equal(needle);
+    });
+  });
+
+  describe('#getSongURI()', function () {
+    const uri = 'spotify:track:5rg2cr5aokhrm5gQNpyHXi';
+    const url =
+      'https://open.spotify.com/track/5rg2cr5aokhrm5gQNpyHXi?si=YBgz1m4IRmC6d1sW6KV6Hw';
+    const parsed = PlaylistManager.getSongURI(url);
+
+    it('should return array', function () {
+      expect(parsed).to.be.a('array');
+    });
+
+    it('should contain uri', function () {
+      expect(parsed[0]).to.equal(uri);
+    });
+  });
+
+  describe('playlist crud', function () {
+    let weeklyPlaylist, freshPlaylist;
+
+    describe('#create()', function () {
+      const today = new Date().toDateString().split(' ').slice(1).join(' ');
+
+      it('should create weekly playlist', async function () {
+        const newPlaylist = await pm.create();
+
+        weeklyPlaylist = newPlaylist;
+
+        expect(newPlaylist).to.not.equal(undefined);
+      });
+
+      it('should be a playlist', function () {
+        expect(weeklyPlaylist.type).to.equal('playlist');
+      });
+
+      it('should have proper description', function () {
+        expect(weeklyPlaylist.description).to.equal(
+          `LS Weekly Meeting playlist for ${today}`,
+        );
+      });
+
+      const month = new Date()
+        .toDateString()
+        .split(' ')
+        .filter((_, idx) => idx % 2 !== 0)
+        .join(' ');
+
+      it('should create monthly playlist', async function () {
+        const newPlaylist = await pm.create(true);
+
+        freshPlaylist = newPlaylist;
+
+        expect(newPlaylist).to.not.equal(undefined);
+      });
+
+      it('should be a playlist', function () {
+        expect(freshPlaylist.type).to.equal('playlist');
+      });
+
+      it('should have proper description', function () {
+        expect(freshPlaylist.description).to.equal(
+          `LS' Fresh Finds for ${month}`,
+        );
+      });
+    });
+
+    describe('#updateCurrent()', function () {
+      it('should update weekly', async function () {
+        const updated = await pm.updateCurrent({ name: 'Sat Apr 4 2022' });
+
+        expect(updated).to.equal(200);
+      });
+
+      it('should update monthly', async function () {
+        const updated = await pm.updateCurrent({ name: 'Mar 2022' }, true);
+
+        expect(updated).to.equal(200);
+      });
+    });
+
+    describe('#addToCurrent()', function () {
+      const album =
+        'https://open.spotify.com/album/2xBerp5gUwSVo6g6OLV7JN?si=q3-rfEbHQz-zr66J_WHLSg';
+      const song =
+        'https://open.spotify.com/track/5rg2cr5aokhrm5gQNpyHXi?si=mR3wLCBlQhGiAV-tVsvF6Q';
+
+      it('should add a song to weekly', async function () {
+        const added = await pm.addToCurrent(song);
+
+        expect(added).to.have.property('snapshot_id');
+      });
+
+      it('should add album to weekly', async function () {
+        const added = await pm.addToCurrent(album);
+
+        expect(added).to.have.property('snapshot_id');
+      });
+
+      it('should add a song to monthly', async function () {
+        const added = await pm.addToCurrent(song, true);
+
+        expect(added).to.have.property('snapshot_id');
+      });
+
+      it('should add album to monthly', async function () {
+        const added = await pm.addToCurrent(album, true);
+
+        expect(added).to.have.property('snapshot_id');
+      });
     });
   });
 });
